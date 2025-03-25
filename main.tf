e0e6815cc658e4f882d2354341dc714e333186c8
Yash Naik <yashtnaik@gmail.com>
2025-03-25 21:16:07 +0530

    yy

--- main.tf
+++ main.tf
@@ -3,7 +3,16 @@ resource "aws_security_group" "yash-sg-dynamic" {
     vpc_id = var.VPC-ID
 
     tags = {
-        Name = "allow_http_traffoc"
+        Name = "Yash-Naik-SG-Terra"
   }
-  
+
+    dynamic "ingress" {
+        for_each = var.ingress-rules
+        content {
+          from_port = ingress.value["from_port"]
+          to_port = ingress.value["to_port"]
+          protocol = ingress.value["protocol"]
+          cidr_blocks = ingress.value["cidr_blocks"]
+        }
+    }
 }
--- terraform.tfvars
+++ terraform.tfvars
@@ -3,6 +3,6 @@ ingress-rules = [
   { from_port = 80, to_port = 80, protocol = "tcp", cidr_blocks = ["0.0.0.0/0"]},
   { from_port = 443, to_port = 443, protocol = "tcp", cidr_blocks = [ "0.0.0.0/0" ]} ]
 
-  SG-name = "yashtnaik-test-sg"
+  SG-name = "yashtnaik-test-sg1"
 
   VPC-ID = "vpc-0be402d855d437f27"
